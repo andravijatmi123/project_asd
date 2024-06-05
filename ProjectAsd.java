c2e61419e67043b4446a93d8b8f012da503e9f56
Andra Vijatmi <andravijatmi1933@gmail.com>
2024-06-05 22:16:55 +0700

    Add files via upload

---
+++ ProjectAsd.java
@@ -0,0 +1,14 @@
+package projectasd;
+import java.util.*;
+public class ProjectAsd {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        Map<String, Integer> pembunuhMap = new HashMap<>();
+        Set<String> kkorbanSet = new HashSet<>();
+        
+        int maksBaris = 999;
+        int barisAwal = 0;
+        int batasKarakterNama = 10;
+    }
+    
+}
